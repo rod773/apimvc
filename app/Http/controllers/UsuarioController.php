7586a7cf60259cc7f473b8fdc531b552440e95b9
rod773 <rodrigoarcadio1@gmail.com>
2024-08-10 16:35:28 +0200

    update

--- app/Http/controllers/UsuarioController.php
+++ app/Http/controllers/UsuarioController.php
@@ -3,8 +3,11 @@
 
 class UsuarioController{
 
-
     public static function create(){
         echo "Esta es la vista de crear";
+    }
+
+    public static function edit($usuario){
+        echo "Vamos a editar al usuario {$usuario}";
     }
 }
